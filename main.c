19d5036723b101a301fb5f9a6b1320d598d90e01
mochizukidaiki <43572081+mochizukidaiki@users.noreply.github.com>
2018-11-05 15:30:00 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 int main(int argc, char *argv[]) 
 {
     char str[7];
-    
+    int a;
     while(fgets(str, sizeof(str), stdin)){
+        scanf("%c",a);
+        str=a;
         printf("%s", str);
     }
 
